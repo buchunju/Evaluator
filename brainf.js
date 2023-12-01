cc9f468b8191c002cc15020a447598f8ed81bb60
unknown <buchunjukenneth@gmail.com>
2023-12-01 12:09:16 +0300

    Passing code as an argument

--- brainf.js
+++ brainf.js
@@ -1,8 +1,29 @@
+const { error } = require("node:console");
+const fs = require("node:fs");
+
 ARRAY_SIZE = 1000;
 
-// brainfuck code
-var code =
-  ">>++++[<+++++[<+++++>-]>-]<<++++.---.>++[<+++>-]<+..+++.>>>++++[<++++[<++>-]>-]<<.--[>++++<-]>-.>++++[<-->-]<.+++.>+++[<-->-]<.>++++[<-->-]<.";
+// check arguments
+if (process.argv.length != 3) {
+  console.log(
+    "\
+    [E] code file required.\n \
+    usage: node brainf.js <filaname>\n\
+  "
+  );
+
+  return 1;
+}
+
+var code = "";
+
+// read code from file
+try {
+  code = fs.readFileSync(process.argv[2], "utf-8");
+} catch (err) {
+  console.log(err.toString());
+  return;
+}
 
 // needed variable
 var array = new Uint8Array(ARRAY_SIZE);
